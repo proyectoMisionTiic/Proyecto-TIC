9d5f347b4a79a24276e5d1ad7b323cf851b9d0bb
Jessica Moncada <jessmoncada1@gmail.com>
2021-09-22 18:30:21 -0500

    Se edita App.js con el registro.html

--- src/App.js
+++ src/App.js
@@ -4,20 +4,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <main>
+    {/* Section para la imagen superior*/} 
+
+    <section>
+    {/*Se crea div para trabajar la imagen como un contenedor y cambiar tamaño*/}
+        <div>
+            <img src="./media/imagen_registro.jpg" alt="Foto de una tienda de ropa"/>
+        </div>
+        
+    </section>
+
+    {/*Section para el registro*/}
+
+    <section>
+              <h3> Registro </h3>
+    {/*Inicia el formulario*/}
+     <form>
+        <label for = "nombre">
+            Nombre :
+            <input name="nombre" type="text"/>
+            <i class="far fa-user-circle"></i>
+        </label>
+        <label for = "email">
+            Email :
+           <input name="email"  type="email"/>
+        </label>
+        <label for = "contraseña">
+            Contraseña :
+           <input name="contraseña" type="password"/>
+        </label>
+        /*Boton para enviar la informacion ingresada por el usuario*/
+        <button type="submit">Registrar</button>
+     </form>
+    </section>
+    
+    </main>
     </div>
   );
 }
