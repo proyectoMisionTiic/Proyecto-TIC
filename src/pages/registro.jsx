b5820b21f3eda11f3195b81367a7bafdaff3d40b
Jessica Moncada <jessmoncada1@gmail.com>
2021-09-24 01:13:26 -0500

    Se importa imagen del registro

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -1,19 +1,21 @@
-
 import imagenRegistro from '../media/imagen_registro.jpg';
 
 function Registro(){
     return(
     <div>
-         
+          
     {/* Section para la imagen superior*/} 
+
     <section>
-    {/*Se crea div para trabajar la imagen como un contenedor y cambiar tamaño*/}
+    
         <div>
-            <img src= {imagenRegistro} alt='Imagen de una tienda de ropa'/>
-        </div> 
+            <img src= {imagenRegistro} alt="Foto de una tienda de ropa"/>
+        </div>
+        
     </section>
 
     {/*Section para el registro*/}
+
     <section>
               <h3> Registro </h3>
     {/*Inicia el formulario*/}
@@ -31,7 +33,6 @@ function Registro(){
             Contraseña :
            <input name="contraseña" type="password"/>
         </label>
-
         {/*Boton para enviar la informacion ingresada por el usuario*/}
         <button type="submit">Registrar</button>
      </form>
