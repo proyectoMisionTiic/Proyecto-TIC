baf0153eab0645afa8c88a6d1c4e38d54eb35021
Jessica Moncada <jessmoncada1@gmail.com>
2021-09-23 23:32:02 -0500

    Se modifica los comentarios

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -2,19 +2,16 @@
 function Registro(){
     return(
     <div>
-          
+         
     {/* Section para la imagen superior*/} 
-
     <section>
     {/*Se crea div para trabajar la imagen como un contenedor y cambiar tamaño*/}
         <div>
-            <img src="./media/imagen_registro.jpg" alt="Foto de una tienda de ropa"/>
-        </div>
-        
+            <img src=" ./media/imagen_registro.jpg" alt="Foto de una tienda de ropa"/>
+        </div> 
     </section>
 
     {/*Section para el registro*/}
-
     <section>
               <h3> Registro </h3>
     {/*Inicia el formulario*/}
@@ -32,6 +29,7 @@ function Registro(){
             Contraseña :
            <input name="contraseña" type="password"/>
         </label>
+
         {/*Boton para enviar la informacion ingresada por el usuario*/}
         <button type="submit">Registrar</button>
      </form>
