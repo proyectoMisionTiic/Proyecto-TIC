d8e66cde09ded2ca2149305b6312974325543ebd
Jessica Moncada <jessmoncada1@gmail.com>
2021-09-30 21:20:51 -0500

    Se agrega boton para cambiar entre ingresar usuario y la tabla

--- src/pages/GestionUsuarios.jsx
+++ src/pages/GestionUsuarios.jsx
@@ -1,126 +1,190 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "../styles/Style-gestionar-productos.css";
-import Nav from '../components/nav';
+const usuariosBackend = [
+  {
+    nombre: "Juan",
+    apellido: "Montoya",
+    cedula: "123456789",
+    email: "juanm@gmail.com",
+    rol: "Administrador",
+    estado: "Pendiente",
+  },
+  {
+    nombre: "Andrea",
+    apellido: "Gomez",
+    cedula: "987654321",
+    email: "andr@hotmail.com",
+    rol: "Vendedo",
+    estado: "Autorizado",
+  },
+];
 
-const GestionUsuarios = () => {
-	return (
-    <body className='flex'>
-      
+const Usuarios = () => {
+  const [mostrarTabla, setMostrarTabla] = useState(true);
+  const [usuarios, setUsuarios] = useState([]);
+  const [textoBoton, setTextoBoton] = useState("Crear Nuevo Usuario");
+
+  useEffect(() => {
+    //obtener lista de usuarios
+    setUsuarios(usuariosBackend);
+  }, []);
+
+  useEffect(() => {
+    if (mostrarTabla) {
+      setTextoBoton("Crear Nuevo Usuario");
+    } else {
+      setTextoBoton("Mostrar Todos los usuarios");
+    }
+  }, [mostrarTabla]);
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-start p-8">
+      <div className="flex flex-col">
+        <button
+          onClick={() => {
+            setMostrarTabla(!mostrarTabla);
+          }}
+          className="text-white bg-gray-800 p-5 rounded-full m-6 w-28 self-end"
+        >
+          {textoBoton}
+        </button>
+      </div>
+      {mostrarTabla ? (
+        <TablaUsuarios listaUsuarios={usuarios} />
+      ) : (
+        <FormularioCreacionUsuarios />
+      )}
+    </div>
+  );
+};
+
+const TablaUsuarios = ({ listaUsuarios }) => {
+  useEffect(() => {
+    console.log(
+      "este es el listado de usuarios en el componente de tabla",
+      listaUsuarios
+    );
+  }, [listaUsuarios]);
+  return (
+    
+    <div className="flex ml-7 mt-7 w-full mr-20">
+          <div class="md:px-32 w-full">
+            <div class="shadow overflow-hidden rounded border-b border-gray-200">
+              <table class="min-w-full bg-white">
+                <thead class="bg-gray-800 text-white">
+                  <tr>
+                    <th class="w-1/3 text-center py-3 px-4 uppercase font-semibold text-sm">
+                      Nombre
+                    </th>
+                    <th class="w-1/3 text-center py-3 px-4 uppercase font-semibold text-sm">
+                      Apellido
+                    </th>
+                    <th class="w-1/3 text-center py-3 px-4 uppercase font-semibold text-sm">
+                      Cedula
+                    </th>
+                    <th class="text-center py-3 px-4 uppercase font-semibold text-sm">
+                      Email
+                    </th>
+                    <th class="text-center py-3 px-4 uppercase font-semibold text-sm">
+                      Rol
+                    </th>
+                    <th class="text-center py-3 px-4 uppercase font-semibold text-sm">
+                      Estado
+                    </th>
+                  </tr>
+                </thead>
+        <tbody>
+          {listaUsuarios.map((usuario) => {
+            return (
+              <tr>
+                <td>{usuario.nombre}</td>
+                <td>{usuario.apellido}</td>
+                <td>{usuario.cedula}</td>
+                <td>{usuario.email}</td>
+                <td>{usuario.rol}</td>
+                <td>{usuario.estado}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+    </div>
+    </div>
+  );
+};
+
+const FormularioCreacionUsuarios = () => {
+  return (
+    <body className="flex">
       <div className="container ml-7 mt-7 max-h-full w-1/2">
         {/* Inicio Seccion de input del usuario */}
         <div className="titulo">Registro de Usuarios</div>
         <form action="#">
           <div className="detalles-producto">
             <div className="input-box">
-              <span className="detalles">ID</span>
-              <input type="number" placeholder="Ingrese el ID " required />
-            </div>
-            <div className="input-box">
               <span className="detalles">Nombre</span>
-              <input type="text" placeholder="Ingrese el nombre " required />
+              <input type="text" placeholder="Ingrese nombres " required />
             </div>
+
             <div className="input-box">
-              <span className="detalles">Descripción</span>
-              <input
-                type="text"
-                placeholder="Ingrese una descripción "
-                required
-              />
+              <span className="detalles">Apellido</span>
+              <input type="text" placeholder="Ingrese apellidos " required />
             </div>
+
             <div className="input-box">
-              <span className="detalles">Valor unitario</span>
-              <input type="number" placeholder="Ingrese el valor " required />
+              <span className="detalles">Cédula </span>
+              <input type="number" placeholder="Ingrese el nombre " required />
             </div>
-            <div className="input-box centrado">
-              <span className="detalles centrado">Cantidad</span>
-              <input
-                type="number"
-                className="centrado"
-                placeholder="Ingrese la cantidad "
-                required
-              />
+
+            <div className="input-box">
+              <span className="detalles">Email</span>
+              <input type="text" placeholder="Ingrese el email " required />
+            </div>
+
+            {/*<div className="input-box">
+            <span className="detalles">Rol</span>
+            <input
+              type="text"
+              placeholder="Ingrese rol"
+              required
+            />
+            */}
+            <div className="input-box">
+              <span className="detalles">Rol</span>
+              <select className="border-2 ">
+                <option value="rol"> Administrador </option>
+                <option value="rol"> Vendedor </option>
+              </select>
+            </div>
+
+            <div className="input-box">
+              <span className="detalles">Estado</span>
+              <select className="border-2">
+                <option value="autorizado"> Autorizado </option>
+                <option value="pendiente"> Pendiente </option>
+                <option value="no autorizado"> No Autorizado </option>
+              </select>
             </div>
           </div>
           {/* Final seccion de input */}
 
           {/* Seccion de ESTADO y BOTONES */}
-          <div className="estado-detalles">
-            <input type="radio" name="estado" id="punto-1" />
-            <input type="radio" name="estado" id="punto-2" />
-            <span className="estado-titulo">Estado</span>
-            <div className="category">
-              <label for="punto-1">
-                <span className="punto uno"></span>
-                <span className="estado">Disponible</span>
-              </label>
-              <label for="punto-2">
-                <span className="punto dos"></span>
-                <span className="estado">No disponible</span>
-              </label>
-            </div>
-          </div>
+
           <div className="botones">
             <div className="button">
-              <input type="submit" value="Buscar por ID" />
+              <input type="submit" value="Buscar por cédula" />
             </div>
             <div className="button">
-              <input type="submit" value="Modificar producto" />
+              <input type="submit" value="Modificar usuario" />
             </div>
             <div className="button">
-              <input type="submit" value="Registrar producto" />
+              <input type="submit" value="Registrar usuario" />
             </div>
           </div>
         </form>
-        {/* Fin seccion de ESTADO y BOTONES */}
       </div>
-      {/* Tabla dummie*/}
-      <section >
-        <div className='flex ml-7 mt-7 w-full mr-20'>
-        <div class="md:px-32 w-full">
-  <div class="shadow overflow-hidden rounded border-b border-gray-200">
-    <table class="min-w-full bg-white">
-      <thead class="bg-gray-800 text-white">
-        <tr>
-          <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Name</th>
-          <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Last name</th>
-          <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Phone</th>
-          <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Email</th>
-        </tr>
-      </thead>
-    <tbody class="text-gray-700">
-      <tr>
-        <td class="w-1/3 text-left py-3 px-4">Lian</td>
-        <td class="w-1/3 text-left py-3 px-4">Smith</td>
-        <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662">622322662</a></td>
-        <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
-      </tr>
-      <tr class="bg-gray-100">
-        <td class="w-1/3 text-left py-3 px-4">Emma</td>
-        <td class="w-1/3 text-left py-3 px-4">Johnson</td>
-        <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662">622322662</a></td>
-        <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
-      </tr>
-      <tr>
-        <td class="w-1/3 text-left py-3 px-4">Oliver</td>
-        <td class="w-1/3 text-left py-3 px-4">Williams</td>
-        <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662">622322662</a></td>
-        <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
-      </tr>
-      <tr class="bg-gray-100">
-        <td class="w-1/3 text-left py-3 px-4">Isabella</td>
-        <td class="w-1/3 text-left py-3 px-4">Brown</td>
-        <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662">622322662</a></td>
-        <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
-      </tr>
-    </tbody>
-    </table>
-  </div>
-</div>
-        </div>
-      </section>
-      {/* Fin tabla */}
-      </body>
-	);
-}
+    </body>
+  );
+};
 
-export default GestionUsuarios
+export default Usuarios;
