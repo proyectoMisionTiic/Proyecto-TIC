cb27dd5c813f4f25733190d4cd28269feaf46325
Jessica Moncada <jessmoncada1@gmail.com>
2021-10-01 21:36:17 -0500

    se emplea ruta para gestionar usuarios

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,75 +1,63 @@
 import Botonsidebar from "./botonsidebar";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  return (
+    <div>
+      <nav className=" celular:bg-gray-800    flex flex-col celular:w-screen   tablet:h-screen  portatil:w-full tablet:w-full portatil:mr-10 tablet:gap-20 portatil:text-3xl  ">
+        <ul>
+          <Link to="/Ventas">
+            <div className=" flex   hover:bg-green-500 my-1 p-2.5 tablet:mt-20 ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+              <i class="fas fa-shopping-cart text-white justify-center mr-3"></i>
+              <Botonsidebar
+                className="celular:"
+                NombreBoton="Gestionar Venta"
+              />
+            </div>
+          </Link>
 
-    return (
+          <Link to="/InformacionVentas">
+            <div className=" flex   hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+              <i class="fas fa-receipt text-white justify-center mr-3"></i>
+              <Botonsidebar
+                className="celular:"
+                NombreBoton="Informacion Ventas"
+              />
+            </div>
+          </Link>
 
-        <div> 
+          <div className=" flex   hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+            <i class="fas fa-tshirt text-white justify-center mr-3"></i>
+            <Botonsidebar className="celular:" NombreBoton="Ver Productos" />
+          </div>
 
-    
-                <nav className=' celular:bg-gray-800    flex flex-col celular:w-screen   tablet:h-screen  portatil:w-full tablet:w-full portatil:mr-10 tablet:gap-20 portatil:text-3xl  '>
+          <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+            <i class="fab fa-shopify text-white justify-center mr-3"></i>
+            <Botonsidebar
+              className="celular:"
+              NombreBoton="Gestionar Producto"
+            />
+          </div>
 
-                <ul>
-            
-                    <Link to="/Ventas">
-                        <div className=' flex   hover:bg-green-500 my-1 p-2.5 tablet:mt-20 ml-2 mr-2 tablet:mb-10 portatil:mb-15'>
-                                <i class="fas fa-shopping-cart text-white justify-center mr-3"></i>
-                                <Botonsidebar className='celular:' NombreBoton='Gestionar Venta' />
-                            </div>
-                    </Link>
-                   
-                 
+          <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-20">
+            <i class="fas fa-sign-out-alt text-white justify-center "></i>
+            <Botonsidebar className="celular:" NombreBoton="Salir" />
+          </div>
 
-            
-
-                    <Link to="/InformacionVentas">
-                        <div className=' flex   hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15'>
-                        <i class="fas fa-receipt text-white justify-center mr-3"></i>
-                        <Botonsidebar className='celular:' NombreBoton='Informacion Ventas' />
-                                    </div>
-                    
-                    </Link>
-
-                    <div className=' flex   hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15'>
-                        <i class="fas fa-tshirt text-white justify-center mr-3"></i>
-                        <Botonsidebar className='celular:' NombreBoton='Ver Productos' />
-                    </div>
-
-                    <div className=' flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15'>
-                        <i class="fab fa-shopify text-white justify-center mr-3"></i>
-                        <Botonsidebar className='celular:' NombreBoton='Gestionar Producto' />
-                    </div>
-
-                    <div className=' flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15'>
-                        <i class="fas fa-users text-white justify-center mr-3"></i>
-                        <Botonsidebar className='celular:' NombreBoton='Gestionar Usuarios' />
-                    </div>
-
-
-                    <div className=' flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-20'>
-                        <i class="fas fa-sign-out-alt text-white justify-center "></i>
-                        <Botonsidebar className='celular:' NombreBoton='Salir' />
-                    </div>
-
-
-                </ul>
-
-                   
-              
-
-            </nav>
-
-
-
-
-
-
-        </div>
-
-    );
-}
-
+          <Link to="/GestionUsuarios">
+            <div className=" flex   hover:bg-green-500 my-1 p-2.5 tablet:mt-20 ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+              <i class="fas fa-shopping-cart text-white justify-center mr-3"></i>
+              <Botonsidebar
+                className="celular:"
+                NombreBoton="Gestionar Usuarios"
+              />
+            </div>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Sidebar;
