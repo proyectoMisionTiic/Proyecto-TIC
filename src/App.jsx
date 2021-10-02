d09be0fbda0432599fdff9278023e8f7e2965373
Jessica Moncada <jessmoncada1@gmail.com>
2021-10-01 20:08:20 -0500

    se cambia app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,57 @@ import "../src/styles/style.css";
 import Layout from "./layouts/layout";
 import React from "react";
 import Principal from "./pages/Principal";
+import InformacionVentas from "./pages/informacionVentas";
+import GestionUsuarios from "./pages/GestionUsuarios";
+import GestionVentas from "./pages/gestionVentas";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./pages/Login";
-import GestionUsuarios from "./pages/GestionUsuarios";
+
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path={['/Principal']}>
+        <Route path={["/Principal", "/Ventas", "/InformacionVentas"]}>
           <Layout>
             <Switch>
-              <Route path='/Principal'>
+              <Route path="/Principal">
                 <Principal />
               </Route>
-            </Switch>
-          </Layout>
-        </Route>
 
-        <Route path={["/Login"]}>
-          <Switch>
-            <Route path="/Login">
-              <Login />
-            </Route>
-          </Switch>
-        </Route>
+              <Route path="/Ventas">
+                <GestionVentas />
+              </Route>
 
-        <Route path={['/Usuarios']}>
-          <Layout>
-            <Switch>
-              <Route path='/Usuarios'>
-                <GestionUsuarios/>
+              <Route path="/InformacionVentas">
+                <InformacionVentas />
               </Route>
             </Switch>
           </Layout>
         </Route>
+        <Route path={["/Principal", "/GestionUsuarios"]}>
+          <Layout>
+            <Switch>
+              <Route path="/Principal">
+                <Principal />
+              </Route>
+
+              <Route path="/Usuarios">
+                <GestionUsuarios />
+              </Route>
+            </Switch>
+          </Layout>
+        </Route>
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+
+          <Route path="/GestionVentas">
+            <GestionVentas />
+          </Route>
+        </Switch>
+
       </Switch>
     </Router>
   );
