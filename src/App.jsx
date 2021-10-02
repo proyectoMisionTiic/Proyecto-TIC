23e008a750495b43c9c09a63c7036aa9bdef723d
Jessica Moncada <jessmoncada1@gmail.com>
2021-10-01 20:39:57 -0500

    se cambia app.jsx, uniendo rama Andrew y Jessica

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
 import "../src/styles/style.css";
 import Layout from "./layouts/layout";
 import React from "react";
-
 import Principal from "./pages/Principal";
 import InformacionVentas from "./pages/informacionVentas";
 import GestionUsuarios from "./pages/GestionUsuarios";
 import GestionVentas from "./pages/gestionVentas";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Principal from './pages/Principal';
-import InformacionVentas from './pages/informacionVentas';
-
-import GestionVentas from "./pages/gestionVentas";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-import Login from './pages/Login'
-
 import Login from "./pages/Login";
 
 function App() {
   return (
-
     <Router>
       <Switch>
-        <Route path={["/Principal", "/Ventas", "/InformacionVentas"]}>
+        <Route path={["/Principal", "/Ventas", "/InformacionVentas",]}>
           <Layout>
             <Switch>
               <Route path="/Principal">
@@ -42,50 +27,11 @@ function App() {
               <Route path="/InformacionVentas">
                 <InformacionVentas />
               </Route>
-            </Switch>
-          </Layout>
-<<<<<<< HEAD
-        </Route>
-        <Route path={["/Principal", "/GestionUsuarios"]}>
-=======
 
-  <Router>
-      <Switch >
-        <Route path={['/Principal','/Ventas','/InformacionVentas']}>
-            <Layout> 
-              <Switch> 
-                  <Route path='/Principal'>
-                    <Principal/>
-                  </Route>
-
-                  <Route path='/Ventas'>
-                    <GestionVentas/>
-                  </Route>
-
-                  <Route path='/InformacionVentas'>
-                    <InformacionVentas/>
-                  </Route>
-
-              </Switch> 
-            </Layout> 
-        </Route>
-
-       
-        <Route path={['/Usuarios']}>
->>>>>>> 8edf8f1f28da2620317153f52ca03feb7cd7ccaa
-          <Layout>
-            <Switch>
-              <Route path="/Principal">
-                <Principal />
-              </Route>
-
-              <Route path="/Usuarios">
-                <GestionUsuarios />
-              </Route>
             </Switch>
           </Layout>
         </Route>
-<<<<<<< HEAD
+
         <Switch>
           <Route path="/">
             <Login />
@@ -95,24 +41,18 @@ function App() {
             <GestionVentas />
           </Route>
         </Switch>
-=======
 
-        
-              <Switch> 
-                  <Route path='/'>
-                    <Login/>
-                  </Route>
-
-                  <Route path='/GestionVentas'>
-                    <GestionVentas/>
-                  </Route>
-
-                 
-              </Switch> 
-        
->>>>>>> 8edf8f1f28da2620317153f52ca03feb7cd7ccaa
 
       </Switch>
+      <Route path={["/GestionUsuarios"]}>
+          <Layout>
+            <Switch>
+              <Route path="/GestionUsuarios">
+                <GestionUsuarios />
+              </Route>
+            </Switch>
+          </Layout>
+        </Route>
     </Router>
   );
 }
