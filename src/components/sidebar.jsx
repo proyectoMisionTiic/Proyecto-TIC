46b431066ad8cc5fe353b2935ba0174933620692
Andrew <84572601+Andew22@users.noreply.github.com>
2021-10-03 15:35:15 -0700

    Update sidebar.jsx

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
       <nav className=" celular:bg-gray-800    flex flex-col celular:w-screen   tablet:h-screen  portatil:w-full tablet:w-full portatil:mr-10 tablet:gap-20 portatil:text-3xl  ">
         <ul>
           <Link to="/Ventas">
-            <div className=" flex   hover:bg-green-500 my-1 p-2.5 tablet:mt-20 ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+            <div className=" flex   hover:bg-green-500 my-1 p-2.5 tablet:mt-20 ml-2 mr-2 tablet:mb-10 portatil:mb-15 andrew-campo">
               <i class="fas fa-shopping-cart text-white justify-center mr-3"></i>
               <Botonsidebar
                 className="celular:"
@@ -19,7 +19,7 @@ const Sidebar = () => {
           </Link>
 
           <Link to="/InformacionVentas">
-            <div className=" flex   hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+            <div className=" flex   hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15 andrew-campo">
               <i class="fas fa-receipt text-white justify-center mr-3"></i>
               <Botonsidebar
                 className="celular:"
@@ -28,13 +28,13 @@ const Sidebar = () => {
             </div>
           </Link>
 
-          <div className=" flex   hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+          <div className=" flex   hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15 andrew-campo">
             <i class="fas fa-tshirt text-white justify-center mr-3"></i>
             <Botonsidebar className="celular:" NombreBoton="Ver Productos" />
           </div>
 
           <Link to="/GestionProductos">
-          <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+          <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15 andrew-campo">
             <i class="fab fa-shopify text-white justify-center mr-3"></i>
             <Botonsidebar
               className="celular:"
@@ -44,7 +44,7 @@ const Sidebar = () => {
           </Link>
 
           <Link to="/GestionUsuarios">
-            <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+            <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15 andrew-campo">
               <i class="fas fa-users text-white justify-center mr-3"></i>
               <Botonsidebar
                 className="celular:"
@@ -54,7 +54,7 @@ const Sidebar = () => {
           </Link>
 
           <Link to="/VerUsuarios">
-            <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15">
+            <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-15 andrew-campo">
               <i class="fas fa-users text-white justify-center mr-3"></i>
               <Botonsidebar
                 className="celular:"
@@ -63,7 +63,7 @@ const Sidebar = () => {
             </div>
           </Link>
 
-          <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-20">
+          <div className=" flex  hover:bg-green-500 my-1 p-2.5  ml-2 mr-2 tablet:mb-10 portatil:mb-20 andrew-campo">
             <i class="fas fa-sign-out-alt text-white justify-center "></i>
             <Botonsidebar className="celular:" NombreBoton="Salir" />
           </div>
@@ -74,4 +74,5 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
 
