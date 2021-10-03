d4fd59b05dbba8b90f63304b31a8c383ab39e40b
Juan Diego Pineda <juand_pineda@hotmail.com>
2021-10-03 18:32:09 -0500

    Formulario editar venta

--- src/pages/informacionVentas.jsx
+++ src/pages/informacionVentas.jsx
@@ -112,7 +112,63 @@ const informacionVentas = () => {
                               <span aria-hidden="true">&times;</span>
                             </button>
                           </div>
-                          <div class="modal-body"></div>
+                          <div class="modal-body">
+                          <form class="w-full max-w-sm align-center">
+                            <div class="md:flex md:items-center mb-6">
+                              <div class="md:w-1/3">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                  Nuevo Valor Total
+                                </label>
+                              </div>
+                              <div class="md:w-2/3">
+                                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number"/>
+                              </div>
+                            </div>
+                            <div class="md:flex md:items-center mb-6">
+                              <div class="md:w-1/3">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                  Nueva fecha
+                                </label>
+                              </div>
+                              <div class="md:w-2/3">
+                                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"/>
+                              </div>
+                            </div>
+                            <div class="md:flex md:items-center mb-6">
+                              <div class="md:w-1/3">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                  Nuevo Documento cliente
+                                </label>
+                              </div>
+                              <div class="md:w-2/3">
+                                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"/>
+                              </div>
+                            </div>
+                            <div class="md:flex md:items-center mb-6">
+                              <div class="md:w-1/3">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                  Nuevo nombre vendedor
+                                </label>
+                              </div>
+                              <div class="md:w-2/3">
+                                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"/>
+                              </div>
+                            </div>
+                            <div class="md:flex md:items-center mb-6">
+                              <div class="md:w-1/3">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                  Nuevo nombre cliente
+                                </label>
+                              </div>
+                              <div class="md:w-2/3">
+                                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"/>
+                              </div>
+                            </div>
+                            
+
+                            
+                          </form>
+                          </div>
                           <div class="modal-footer">
                             <button
                               type="button"
