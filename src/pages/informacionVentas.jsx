318ca78a547c36a0243dd94273f4ed5dc6effc37
Juan Diego Pineda <juand_pineda@hotmail.com>
2021-10-03 17:58:02 -0500

    Confirmacion de edicion de ventas

--- src/pages/informacionVentas.jsx
+++ src/pages/informacionVentas.jsx
@@ -1,9 +1,22 @@
-
+import { ToastContainer, toast } from "react-toastify";
 import React from 'react'
 import "../styles/Style-ventas.css";
-import Nav from '../components/nav';
+
+
+const mostrarMensaje = () => {
+  toast.success("Venta editada correctamente", {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
 
 const informacionVentas = () => {
+
     return (
       <div >
   
@@ -25,6 +38,7 @@ const informacionVentas = () => {
                     <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Nombre Vendedor</th>
                     <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Nombre Cliente</th>
                     <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Productos</th>
+                    <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Acciones</th>
                    
                     
                   </tr>
@@ -39,7 +53,70 @@ const informacionVentas = () => {
                     <td class=" text-left py-3 px-4">0</td>
                     <td class=" text-left py-3 px-4">0</td>
                     <td class=" text-left py-3 px-4">0</td>
-  
+                    <td class=" text-left py-3 px-4">
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      data-toggle="modal"
+                      data-target="#exampleModal"
+                    >
+                      Editar
+                    </button>
+                    {/* Modal del boton editar */}
+                    <div
+                      class="modal fade"
+                      id="exampleModal"
+                      tabindex="-1"
+                      role="dialog"
+                      aria-labelledby="exampleModalLabel"
+                      aria-hidden="true"
+                    >
+                      <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">
+                              Editar venta
+                            </h5>
+                            <button
+                              type="button"
+                              class="close"
+                              data-dismiss="modal"
+                              aria-label="Close"
+                            >
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body">
+                          
+                          </div>
+                          <div class="modal-footer">
+                            <button
+                              type="button"
+                              class="btn btn-secondary"
+                              data-dismiss="modal"
+                            >
+                              Cerrar
+                            </button>
+                            <button type="submit" value="Guardar" class="btn btn-primary" onClick={mostrarMensaje}>
+                              Guardar cambios
+                            </button>
+                          </div>
+                        </div>
+                        <ToastContainer
+                            position="top-center"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                          />
+                      </div>
+                      {/* fin modal */}
+                    </div>
+                  </td>
                   </tr>
                 </tbody>
               </table>
