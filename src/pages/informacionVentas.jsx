a36f0212f056887324edeb27816739b8e8bd28db
Andrew <84572601+Andew22@users.noreply.github.com>
2021-10-03 15:36:12 -0700

    Update informacionVentas.jsx

--- src/pages/informacionVentas.jsx
+++ src/pages/informacionVentas.jsx
@@ -1,129 +1,66 @@
-import React from "react";
+
+import React from 'react'
 import "../styles/Style-ventas.css";
-import Nav from "../components/nav";
+import Nav from '../components/nav';
 
 const informacionVentas = () => {
-  return (
-    <div>
-      <div className="andrew-container bg-gray-900">
-        {/* Inicio Seccion de input del usuario */}
+    return (
+      <div >
+  
+        <div className="andrew-container bg-gray-900">
+          {/* Inicio Seccion de input del usuario */}
+          <div className="andrew-titulo mb-10 ">Informacion Ventas</div>
+  
+           <form  className='' action="#">
+       
+            <div className="andrew-detalles-producto ">
+  
+              <table class="min-w-full bg-white">
+                <thead class="bg-gray-800 text-white">
+                  <tr>
+                    <th class=" text-left py-3 px-4 uppercase font-semibold text-sm">ID Venta</th>
+                    <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Valor Total</th>
+                    <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Fecha De Venta</th>
+                    <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Documento Cliente</th>
+                    <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Nombre Vendedor</th>
+                    <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Nombre Cliente</th>
+                    <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">Productos</th>
+                   
+                    
+                  </tr>
+                </thead>
+                <tbody class="text-gray-700">
+  
+                  <tr class="bg-gray-100">
+                    <td class=" text-left py-3 px-4">0</td>
+                    <td class=" text-left py-3 px-4">0</td>
+                    <td class=" text-left py-3 px-4">0</td>
+                    <td class=" text-left py-3 px-4">0</td>
+                    <td class=" text-left py-3 px-4">0</td>
+                    <td class=" text-left py-3 px-4">0</td>
+                    <td class=" text-left py-3 px-4">0</td>
+  
+                  </tr>
+                </tbody>
+              </table>
+  
+  
 
-        <div className="andrew-titulo mb-10 ">Informacion Ventas</div>
-
-        {/* Barra de busqueda */}
-        <div className="flex p-6 w-1/4 -m-5 ">
-          <div className="bg-white flex  items-center  shadow-xl ">
-            <input
-              className=" w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
-              id="search"
-              type="number"
-              placeholder="Búsqueda por id"
-            />
-            <div className="p-4">
-              <button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
-                <i class="bx bx-search text-3xl"></i>
-              </button>
             </div>
-          </div>
-        </div>
-        {/* fin barra */}
-        <form className="" action="#">
-          <div className="andrew-detalles-producto ">
-            <table class="min-w-full bg-white">
-              <thead class="bg-gray-800 text-white">
-                <tr>
-                  <th class=" text-left py-3 px-4 uppercase font-semibold text-sm">
-                    ID Venta
-                  </th>
-                  <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">
-                    Valor Total
-                  </th>
-                  <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">
-                    Fecha De Venta
-                  </th>
-                  <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">
-                    Documento Cliente
-                  </th>
-                  <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">
-                    Nombre Vendedor
-                  </th>
-                  <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">
-                    Nombre Cliente
-                  </th>
-                  <th class="  text-left py-3 px-4 uppercase font-semibold text-sm">
-                    Acciones
-                  </th>
-                </tr>
-              </thead>
-              <tbody class="text-gray-700">
-                <tr class="bg-gray-100">
-                  <td class=" text-left py-3 px-4">0</td>
-                  <td class=" text-left py-3 px-4">0</td>
-                  <td class=" text-left py-3 px-4">0</td>
-                  <td class=" text-left py-3 px-4">0</td>
-                  <td class=" text-left py-3 px-4">0</td>
-                  <td class=" text-left py-3 px-4">0</td>
-                  <td class=" text-left py-3 px-4">
-                    <button
-                      type="button"
-                      className="btn btn-primary"
-                      data-toggle="modal"
-                      data-target="#exampleModal"
-                    >
-                      Editar
-                    </button>
-                    {/* Modal del boton editar */}
-                    <div
-                      class="modal fade"
-                      id="exampleModal"
-                      tabindex="-1"
-                      role="dialog"
-                      aria-labelledby="exampleModalLabel"
-                      aria-hidden="true"
-                    >
-                      <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">
-                              Editar venta
-                            </h5>
-                            <button
-                              type="button"
-                              class="close"
-                              data-dismiss="modal"
-                              aria-label="Close"
-                            >
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-                          
-                          </div>
-                          <div class="modal-footer">
-                            <button
-                              type="button"
-                              class="btn btn-secondary"
-                              data-dismiss="modal"
-                            >
-                              Cerrar
-                            </button>
-                            <button type="button" class="btn btn-primary">
-                              Guardar cambios
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                      {/* fin modal */}
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </form>
+          </form>
+        
       </div>
-    </div>
-  );
-};
-
-export default informacionVentas;
+      </div>
+  
+  
+  
+  
+  
+  
+  
+  
+    );
+  }
+  
+  export default informacionVentas;
+  
